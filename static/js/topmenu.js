74e6d2931a240b4afd2dab461f2df9f497469f08
unknown <surya.thogaru5@gmail.com>
2014-07-08 15:13:38 +0530

    Webpage

---
+++ static/js/topmenu.js
@@ -0,0 +1,20 @@
+function events()
+{
+	window.open("events.html","_self");
+}
+function team()
+{
+	window.open("team.html","_self");
+}
+function gallery()
+{
+	window.open("gallery.html","_self");
+}
+function contactus()
+{
+	window.open("ContactUs.html","_self");
+}
+function home()
+{
+	window.open("quizclub.html","_self");
+}
